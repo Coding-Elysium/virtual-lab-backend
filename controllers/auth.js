39188965a74231ac50070f159411748dfc4dbf2e
ElysiumsGit <builtdif07@gmail.com>
2025-07-05 12:51:23 +0800

    UPDATE LOGIN

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import Admin from "../schema/AdminModel.js";
 import Student from "../schema/StudentModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const loginStudentController = async (req, res) => {
   try {
@@ -31,9 +32,7 @@ export const loginAdminController = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Email and password are required" });
+      return res.status(400).json({ message: "Email and password are required" });
     }
 
     const user = await Admin.findOne({ email });
@@ -46,7 +45,20 @@ export const loginAdminController = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    res.status(200).json({ message: "Login successful", user });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      admin: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+      token,
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
