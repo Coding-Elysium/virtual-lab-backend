7bb36ba47d54c1f15eae2d6cc60bf5dc82b8ed22
John Carlo <abierajc0707@gmail.com>
2025-07-12 16:22:33 +0800

    CORS MODIFY

--- index.js
+++ index.js
@@ -14,21 +14,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:4173",
-  "https://virtual-lab-frontend-deployed-hku1.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:4173",
+//   "https://virtual-lab-frontend-deployed-hku1.vercel.app",
+// ];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
