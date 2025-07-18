665940fcc114794cc36d1815db0283c232dd8b1e
John Carlo <abierajc0707@gmail.com>
2025-07-18 21:31:44 +0800

    DEPLOY

--- schema/InventoryModel.js
+++ schema/InventoryModel.js
@@ -13,7 +13,7 @@ const itemSchema = new mongoose.Schema(
       trim: true,
     },
   },
-  { _id: false } 
+  { _id: false }
 );
 
 const inventorySchema = new mongoose.Schema(
@@ -24,8 +24,8 @@ const inventorySchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ['coc1', 'coc2', 'coc3'],
-      required: true
+      enum: ["coc1", "coc2", "coc3"],
+      required: true,
     },
     studentId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -43,3 +43,22 @@ const inventorySchema = new mongoose.Schema(
 const Inventory = mongoose.model("Inventory", inventorySchema);
 
 export default Inventory;
+
+// {
+//     "take": "take_one",
+//     "type": "coc1",
+//     "studentId": "studentId",
+//     "ingredients": [
+//       {
+//         "path": "/documents/coc1/form1.png",
+//         "name": "chicken"
+//       },
+//       {
+//         "path": "/documents/coc1/receipt1.jpg",
+//         "name": "pork"
+//       }
+//     ]
+//   },
+
+//   BASEURL/inventory/create
+//   BASEURL/read/${studentId}/?type=coc1&take=take_one
