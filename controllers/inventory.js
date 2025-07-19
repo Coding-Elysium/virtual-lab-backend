60482d01066caa7ce3c2046ea4806414c2b73402
John Carlo <abierajc0707@gmail.com>
2025-07-19 14:46:15 +0800

    CARLO

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -4,21 +4,26 @@ export const addInventory = async (req, res) => {
   try {
     const { take, type, studentId, ingredients } = req.body;
 
-    if (!type, !studentId || !Array.isArray(ingredients) || ingredients.length === 0) {
+    if (
+      (!type,
+      !studentId || !Array.isArray(ingredients) || ingredients.length === 0)
+    ) {
       return res.status(400).json({ message: "Invalid input data" });
     }
 
-    const newInventory = await Inventory.create({
-        take,
-        type,
-        studentId,
-        ingredients,
+    await Inventory.create({
+      take,
+      type,
+      studentId,
+      ingredients,
     });
 
-    return res.status(201).json(newInventory);
+    return res
+      .status(201)
+      .json({ success: true, message: "Inventory Success" });
   } catch (error) {
     console.error("Error adding inventorys:", error);
-    return res.status(500).json({ message: "Server error" });
+    return res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
@@ -30,12 +35,16 @@ export const getInventory = async (req, res) => {
     const data = await Inventory.findOne({ take, type, studentId: id });
 
     if (!data) {
-      return res.status(404).json({ success: false, message: "Inventory not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Inventory not found" });
     }
 
-    return res.status(200).json({ success: true, message: "Success", items: data });
+    return res
+      .status(200)
+      .json({ success: true, message: "Get Inventory Success" });
   } catch (error) {
     console.error("Error fetching inventory:", error);
-    return res.status(500).json({ message: "Server error" });
+    return res.status(500).json({ success: false, message: "Server error" });
   }
 };
