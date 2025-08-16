25ccf09fc1791d233d53261ecb3fb23f0aa70ed0
John Carlo <abierajc0707@gmail.com>
2025-08-16 09:41:59 +0800

    CARLO

--- routes/stageRoute.js
+++ routes/stageRoute.js
@@ -3,6 +3,6 @@ import { stageController } from "../controllers/stage.js";
 
 const router = express.Router();
 
-router.get("/read", stageController);
+router.get("/read/:studentId", stageController);
 
 export default router;
